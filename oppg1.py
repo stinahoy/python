1a33354a10d894387cec14cc5aa668e47e1da1fc
stinahoy <stinaho@hiof.no>
2024-09-11 14:25:20 +0200

    10.09

---
+++ oppg1.py
@@ -0,0 +1,25 @@
+
+planeter = ["Merkur", "Venus", "Jorden", "Saturn", "Uranus"]
+print(planeter)
+
+print(planeter,[2])
+
+planeter[3] = "Mars"
+print(planeter)
+
+planeter.append("Neptun")
+priny(planeter)
+planeter.append("Pluto")
+print(planeter)
+
+
+
+random_list = ["Europe", 7, ["Ny liste", 23, "Med nye elementer"], "Bil"]
+
+print(random_list[2])
+print(random_list[2][2])
+random_list[2][2] = "Forelesning"
+print(random_list[2])
+
+random_list[2].insert(_index 1, _object: "Spørsmål i timen")
+print(random_list)
