ba794eb35be21e5015bee3f04347a7764a790406
stinahoy <stinaho@hiof.no>
2024-09-13 10:26:16 +0200

    1209

---
+++ Oblig1_Stina-Hoyer.zip /oppgave3.py
@@ -0,0 +1,2 @@
+minikakulator = 5 * 6 / 3 + 80 - 16 % 5 ** 5 // 3
+print(minikakulator)
---
+++ Oblig1_Stina-Hoyer.zip /oppgave4.py
@@ -0,0 +1,7 @@
+a = 6
+b = 3
+c = 2
+print(a + b * c)
+print((a + b) * c)
+print(a / b / c)
+print(a / (b / c))
