7ee5593212a699124cd1dc3f767bbc2b4df8673c
stinahoy <stinaho@hiof.no>
2024-09-27 17:42:53 +0200

    .

--- oblig2_stina_hoyer/oppgave3.py
+++ oblig2_stina_hoyer/oppgave3.py
@@ -1,6 +1,10 @@
 tolkenboker= ["The Hobbit", "Frmer Giles of Ham", "The Fellowship of the Ring", "The two Towers", "The Return of the King", "The Adventures of Tom Bombadil", "Tree anf Leaf"]
 
-print()
-print()
+print("De to første bøkene:", tolkenboker[:2])
+print("De to siste bøkene:", tolkenboker[-2:])
 
 tolkenboker += ["The Silmarillion", "Unfinished Tales"]
+
+tolkenboker[2:5] = ["Lord of the rings: {title}" for title in tolkenboker[2:5]]
+
+print(sorted(tolkenboker))
